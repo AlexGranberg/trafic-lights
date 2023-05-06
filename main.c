3f85443a4d68e86eea29c119fd9c1fcf34fcfaea
AlexGranberg <alexandergranberg1983@gmail.com>
2023-05-06 17:02:52 +0200

    working traffic lights with millis() function

--- main.c
+++ main.c
@@ -6,79 +6,72 @@
 #define BIT_SET(a, b) ((a) |= (1ULL << (b)))
 #define BIT_CLEAR(a,b) ((a) &= ~(1ULL<<(b)))
 #define BIT_FLIP(a,b) ((a) ^= (1ULL<<(b)))
-#define BIT_CHECK(a,b) (!!((a) & (1ULL<<(b)))) 
+#define BIT_CHECK(a,b) (!!((a) & (1ULL<<(b))))
 
 #define YELLOW 2
 #define RED 3
 #define GREEN 1
 
-void trafficLights(){
+void trafficLights() {
+    static unsigned long prev_millis = 0;
+    static int state = GREEN;
+    unsigned long current_millis = millis();
+    unsigned long elapsed_millis = current_millis - prev_millis;
 
-
-
+    switch (state) {
+        case GREEN:
+            if (elapsed_millis >= 2000) {
+                BIT_CLEAR(PORTB, YELLOW);
+                BIT_CLEAR(PORTB, RED);
+                BIT_SET(PORTB, GREEN);
+                state = YELLOW;
+                prev_millis = current_millis;
+            }
+            break;
+        case YELLOW:
+            if (elapsed_millis >= 8000) {
+                if (BIT_CHECK(PORTB, GREEN)) {
+                    BIT_CLEAR(PORTB, GREEN);
+					BIT_CLEAR(PORTB, RED);
+                    BIT_SET(PORTB, YELLOW);
+                    state = RED;
+                    prev_millis = current_millis;
+                } else {
+                    BIT_CLEAR(PORTB, GREEN);
+					BIT_CLEAR(PORTB, RED);
+                    BIT_SET(PORTB, YELLOW);
+                    state = GREEN;
+                    prev_millis = current_millis;
+                }
+            }
+            break;
+        case RED:
+            if (elapsed_millis >= 2000) {
+                BIT_CLEAR(PORTB, GREEN);
+                BIT_CLEAR(PORTB, YELLOW);
+                BIT_SET(PORTB, RED);
+                state = YELLOW;
+                prev_millis = current_millis;
+            }
+            break;
+    }
 }
 
-int main(void)
-{
+int main(void) {
+    init_millis(16000000UL);
+    sei();
 
-	init_millis(16000000UL);
-	unsigned long prev_millis1;
-	unsigned long prev_millis2;
-	unsigned long prev_millis3;
-	unsigned long prev_millis4;
-	prev_millis1 = millis();
-	prev_millis2 = millis();
-	prev_millis3 = millis();
-	prev_millis4 = millis();
-	sei();
+    BIT_SET(DDRB, YELLOW);
+    BIT_SET(DDRB, RED);
+    BIT_SET(DDRB, GREEN);
 
-BIT_SET(DDRB, YELLOW);
-BIT_SET(DDRB, RED);
-BIT_SET(DDRB, GREEN);
+    BIT_CLEAR(PORTB, YELLOW);
+    BIT_CLEAR(PORTB, RED);
+    BIT_SET(PORTB, GREEN);
 
-while(1){
-	if(millis() - prev_millis1 > 1000){
-		BIT_SET(PORTB, GREEN);
-		BIT_CLEAR(PORTB, YELLOW);
-		BIT_CLEAR(PORTB, RED);
-		prev_millis1 = millis();
-			if(millis() - prev_millis2 > 3000){
-				BIT_CLEAR(PORTB, GREEN);
-				BIT_SET(PORTB, YELLOW);
-				BIT_CLEAR(PORTB, RED);
-				prev_millis1 = millis();
-					if(millis() - prev_millis3 > 7000){
-						BIT_CLEAR(PORTB, GREEN);
-						BIT_CLEAR(PORTB, YELLOW);
-						BIT_SET(PORTB, RED);
-						prev_millis1 = millis();
-							if(millis() - prev_millis4 > 11000){
-								BIT_CLEAR(PORTB, GREEN);
-								BIT_SET(PORTB, YELLOW);
-								BIT_CLEAR(PORTB, RED);
-								prev_millis1 = millis();
-							}
-					}
-			}
-	}
-	// else if(millis() - prev_millis2 > 3000){
-	// 		BIT_CLEAR(PORTB, GREEN);
-	// 		BIT_SET(PORTB, YELLOW);
-	// 		BIT_CLEAR(PORTB, RED);
-	// 		prev_millis2 = millis();
-	// // }
-	// else if(millis() - prev_millis3 > 3000){
-	// 		BIT_CLEAR(PORTB, GREEN);
-	// 		BIT_CLEAR(PORTB, YELLOW);
-	// 		BIT_SET(PORTB, RED);
-	// 		prev_millis4 = millis();
-	// // }
-	// else if(millis() - prev_millis4 > 3000){
-	// 		BIT_CLEAR(PORTB, GREEN);
-	// 		BIT_SET(PORTB, YELLOW);
-	// 		BIT_CLEAR(PORTB, RED);
-	// 		prev_millis2 = millis();
-	// 	}
-	}
-	return 0;
+    while (1) {
+        trafficLights();
+    }
+
+    return 0;
 }
